caddac5ef6f15f25af20aaf6b6c088686a5c45a7
Nazarov Petr <mrzueck@gmail.com>
2019-07-06 22:17:20 +1000

    clean router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Fragment } from "react";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
+import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -14,39 +13,24 @@ const App = () => {
   return (
     <GithubState>
       <AlertState>
-      <Router>
-        <div className="App">
-          <Navbar title="Github Finder" />
-          <Alert />
+        <Router>
+          <div className="App">
+            <Navbar title="Github Finder" />
+            <Alert />
 
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <Fragment>
-                  <Search
-                  />
-                  <Users />
-                </Fragment>
-              )}
-            />
-            <Route exact path="/about" component={About} />
-            <Route
-              exact
-              path="/user/:login"
-              render={props => (
-                <User
-                  {...props}
-                />
-              )}
-            />
-          </Switch>
-        </div>
-      </Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => <User {...props} />}
+              />
+            </Switch>
+          </div>
+        </Router>
       </AlertState>
     </GithubState>
-    
   );
 };
 
